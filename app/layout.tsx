fb4bef3d7c56b72a9dba8cadc04016c364891d89
Shinobu <jcalonso015@gmail.com>
2023-08-29 12:29:50 -0400

    add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-const inter = Inter({ subsets: ['latin'] })
-
+const font = Open_Sans({ subsets: ['latin'] })
+import {ThemeProvider} from '../components/providers/theme-provider'
+import {cn} from '@/lib/utils'
 export const metadata: Metadata = {
   title: 'JC Private Discord',
   description: 'A private discord built by JC',
@@ -16,8 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-<html lang="en">
-      <body className={inter.className}>{children}</body>
+<html lang="en" suppressHydrationWarning>
+      <body className={cn(font.className, "white:bg-[#f5f5f5] dark:bg-[#313338]")}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-theme">
+        {children}
+        </ThemeProvider>
+     
+        
+        </body>
     </html>
     </ClerkProvider>
  
